d9092de0f7e65617b3f3aae4e7d6acd88bbfe4dd
shcampos <samuel-hc@hotmail.com>
2020-09-05 19:26:47 -0400

    feat: forms work

--- src/App.js
+++ src/App.js
@@ -7,28 +7,25 @@ import {
   AccordionDetails,
   Button,
   FormControl,
-  FormControlLabel,
   Grid,
   InputLabel,
   InputAdornment,
   MenuItem,
   OutlinedInput,
   Select,
-  Switch,
   TextField,
   Typography } from '@material-ui/core';
-import {
-  Autocomplete
-  } from '@material-ui/lab';
+
+import { Alert, AlertTitle, Autocomplete } from '@material-ui/lab';
 import { ExpandMore } from '@material-ui/icons'
 
-import useWindowDimensions from './utils/useWindowDimensions';
-import leafForms from './utils/leafForms.json'
+import useWindowDimensions from './utils/useWindowDimensions'
 
 export default function App() {
   const { height, width } = useWindowDimensions();
   const useStyles = makeStyles((theme) => ({
     root: {
+      paddingTop: '50px',
       flexGrow: 1,
       width: width,
       backgroundColor: '#fefefe'
@@ -43,43 +40,72 @@ export default function App() {
   }));
 
   const classes = useStyles();
-  const [expanded, setExpanded] = useState('panel1');
+  const [expanded, setExpanded] = useState(false)
   const [searched, setSearched] = useState(false)
-  const [hasInflorescence, setHasInflorescence] = useState(false)
+  const [familly, setFamilly] = useState('')
+  const [genus, setGenus] = useState('')
+  const [searchParams, setSearchParams] = useState({ })
+
   const handlePanelChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
+
+  const handleFormSearchChange = (event) => {
+    const auxValues = { ...searchParams };
+    auxValues[event.target.name] = event.target.value;
+    setSearchParams(auxValues);
+  };
+
+  const handleSearchSubmit = callback => event => {
+    event.preventDefault();
+    callback();
+  };
+
+  const searchSpecie = () => {
+    setSearched(true)
+  }
+
 
   return (
     <Grid container direction="column" justify="center" alignItems="center" className={classes.root}>
       <Typography variant="h2">
         Lineus
       </Typography>
-      <Typography>
+      <Typography variant="h6">
         Apenas um site para ajudar na identificação de plantas
       </Typography>
 
       <div>
-        <Accordion>
-          <AccordionSummary expanded={expanded === 'panel1'} onChange={handlePanelChange('panel1')}
-          expandIcon={<ExpandMore />} aria-controls="panel1a-content" id="panel1a-header">
-            <Typography className={classes.heading}>Pesquisar uma espécie</Typography>
+        <Accordion expanded={expanded === 'panel1'} onChange={handlePanelChange('panel1')}>
+          <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel1a-content" id="panel1a-header">
+            <Typography className={classes.heading} variant="overline">
+              Pesquisar uma espécie
+            </Typography>
           </AccordionSummary>
           <AccordionDetails fullWidth>
             {!searched?(
-              <form item autoComplete="off" style={{width: '100%'}}>
+              <form item autoComplete="off" style={{width: '100%'}} onSubmit={handleSearchSubmit(searchSpecie)}>
                 <Grid container spacing={4}>
                   <Grid item xs={6}>
-                    <Autocomplete                      
+                    <Autocomplete
+                      required
                       id="familly"
+                      value={familly}
+                      onChange={(event, newValue) => {
+                        setFamilly(newValue);
+                      }}
                       options={["balba", "blabla"]}
-                      getOptionLabel={(option) => option}
                       renderInput={(params) => <TextField {...params} label="Família" variant="outlined" />}
                     />
                   </Grid>
                   <Grid item xs={6}>
                     <Autocomplete
+                      required
                       id="genus"
+                      value={genus}
+                      onChange={(event, newValue) => {
+                        setGenus(newValue)
+                      }}
                       options={["balba", "blabla"]}
                       getOptionLabel={(option) => option}
                       renderInput={(params) => <TextField {...params} label="Gênero" variant="outlined" />}
@@ -90,8 +116,12 @@ export default function App() {
                   <Grid item xs={6}>
                     <FormControl fullWidth variant="outlined" className={classes.input}>
                       <InputLabel id="habitoLabel">Hábito</InputLabel>
-                      <Select labelId="habitoLabel" id="habito"
-                      /**value={age} onChange={handleChange}**/ >
+                      <Select
+                        required
+                        labelId="habitoLabel"
+                        id="habito"
+                        name="habito"
+                        onChange={handleFormSearchChange}>
                         <MenuItem value={'Árvore'}>Árvore</MenuItem>
                         <MenuItem value={'Volúvel'}>Volúvel</MenuItem>
                         <MenuItem value={'Erva'}>Erva</MenuItem>
@@ -102,9 +132,13 @@ export default function App() {
                   <Grid item xs={6}>
                     <FormControl fullWidth variant="outlined" className={classes.input}>
                       <InputLabel id="alturaLabel">Altura</InputLabel>
-                      <OutlinedInput                  
+                      <OutlinedInput
+                        required
                         id="altura"
+                        name="altura"
+                        onChange={handleFormSearchChange}
                         labelId="alturaLabel"
+                        type="number"
                         //value={values.amount}
                         //onChange={handleChange('amount')}
                         endAdornment={<InputAdornment position="start">m</InputAdornment>}
@@ -114,25 +148,52 @@ export default function App() {
                 </Grid>
                 
                 <Typography variant="overline">Folha</Typography>
-                <TextField fullWidth multiline rows={5} id="newSpecieDescription" className={classes.input} label="Descrição" variant="outlined"/>
+                <TextField 
+                  required
+                  id="descricaoFolha"
+                  name="descricaoFolha"
+                  onChange={handleFormSearchChange}
+                  fullWidth
+                  multiline
+                  rows={5}                  
+                  className={classes.input}
+                  label="Descrição"
+                  variant="outlined"
+                />
 
                 <Typography variant="overline">Flor e inflorescência (se tiver)</Typography>
-                <TextField fullWidth multiline rows={5} id="newSpecieDescription" className={classes.input} label="Descrição" variant="outlined"/>
+                <TextField
+                  required
+                  id="descricaoFlor"
+                  name="descricaoFlor"
+                  onChange={handleFormSearchChange}
+                  fullWidth
+                  multiline
+                  rows={5}                  
+                  className={classes.input}
+                  label="Descrição"
+                  variant="outlined"
+                />
 
-                <Button variant="contained" color="primary">
+                <Button variant="contained" color="primary" type="submit">
                   Pesquisar
                 </Button>
               </form>
             ):(
-              <Typography>Possíveis espécies: </Typography>
-            )}            
+              <Alert severity="info" style={{width: '100%'}}>
+                <AlertTitle>Info</AlertTitle>
+                This is an info alert — <strong>check it out!</strong>
+              </Alert>
+            )}
           </AccordionDetails>
         </Accordion>
 
-        <Accordion>
-          <AccordionSummary expanded={expanded === 'panel2'} onChange={handlePanelChange('panel2')}
+        <Accordion expanded={expanded === 'panel2'} onChange={handlePanelChange('panel2')}>
+          <AccordionSummary
           expandIcon={<ExpandMore />} aria-controls="panel1a-content" id="panel1a-header">
-            <Typography className={classes.heading}>Adicionar uma espécie</Typography>
+            <Typography className={classes.heading} variant="overline">
+              Adicionar uma espécie
+            </Typography>
           </AccordionSummary>
           <AccordionDetails>
             <form autoComplete="off" style={{width: '100%'}}>
