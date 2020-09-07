a462579c6831eec5ab1a6250eeb5ee85ee3ba3c2
shcampos <samuel-hc@hotmail.com>
2020-09-07 11:41:27 -0400

    feat: .env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,13 +2,13 @@ const firebase = require('firebase/app')
 require('firebase/database')
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDULUALMQA7hXa7pfgArN9u0QFd6tPEN_s",
-	authDomain: "especies-pantanal.firebaseapp.com",
-	databaseURL: "https://especies-pantanal.firebaseio.com",
-	projectId: "especies-pantanal",
-	storageBucket: "especies-pantanal.appspot.com",
-	messagingSenderId: "201819029776",
-	appId: "1:201819029776:web:ab0ffe3c0a2b33bb92e595"
+	apiKey: process.env.FIREBASE_APIKEY,
+	authDomain: process.env.FIREBASE_AUTHDOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASEURL,
+	projectId: process.env.FIREBASE_PROJECTID,
+	storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+	appId: process.env.FIREBASE_APID
 }
 firebase.initializeApp(firebaseConfig)
 
@@ -17,3 +17,10 @@ export async function postDescription(data) {
 	.then(()=>true)
 	.catch(()=>false)
 }
+
+
+
+
+
+
+
